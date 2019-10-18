0e06d28c05229a5aafd6647cedfbb879b0780a41
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-18 15:41:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,13 +22,17 @@ class Scraper
       #binding.pry
   end
   
-  def get_courses
+  def make_courses
     self.css(".post").each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
+  end
+  
+  def get_courses
+    self.get_page.css('.post')
   end
   
 end
